f405e26f1c40af9becd57d7e61394488612cbb13
Benjamin Stahl <benstahl92@gmail.com>
2018-04-26 13:43:28 -0700

    testing added to Spectrum class

--- final_project/fp.py
+++ final_project/fp.py
@@ -30,6 +30,7 @@ def mysql_query(usr, pswd, db, query):
 
     Doctests/Examples
     -----------------
+    # NB: these tests will fail unless usr, pswd, and db are the valid credentials for my research group's database
     >>> query = "SELECT t1.ObjName FROM objects as t1, spectra as t2 WHERE (t1.ObjID = t2.ObjID) AND (t2.UT_Date > 19850101) AND (t2.UT_Date < 20180101) AND (t2.Min < 4500) and (t2.Max > 7000) AND (t2.Filename NOT LIKE '%gal%');"
     >>> result = mysql_query(dbp.usr, dbp.pswd, dbp.db, query)
     >>> len(result)
@@ -62,48 +63,138 @@ def mysql_query(usr, pswd, db, query):
 
 class Spectrum:
     '''
-    base class for each spectrum to be analyzed that provides a self-contained pathway from metadata to processed spectra
+    base class for spectra that provides a self-contained pathway from metadata to various data products
+        class contains methods for direct use by class instances and functions that can be exposed outside of a specific instance
+        for examples/doctests see docstrings for the methods and functions of the class
 
     General Concepts
     ----------------
-    spec : 2d array of (column-wise) wavelength, flux [wherever spec is used, it should be assumed to be in this form unless explicitly noted as otherwise]
+    spec : 2d array of (column-wise) wavelength, flux [spec should be assumed to be in this form unless explicitly noted as otherwise]
 
+    Methods
+    -------
+    __init__ : instantiation instructions
+    preprocess : performs preprocessing procedure on spectrum
+
+    Functions
+    ---------
+    read_file : reads (and returns) spectrum from provided file (with relative path)
+    dez : de-redshifts (and returns) given spectrum
+    sp_medfilt : wrapper around scipy medfilt function
+    log_bin : bins spectrum to logarithmic scale and returns re-binned spectrum
+    cont_subtr : fits cubic spline to subset of points in given spectrum and subtracts it to remove a pseudo continuum
+    apodize : tapers edges of spectrum using a Hanning window
+    flux_norm : normalizes flux (f) according to the algorithm: (f - f_min) / (f_max - f_min) - mean(qty on left)
     '''
 
-    def __init__(self, sn_name, sn_subtype, spec_filename, redshift, SNR):
+    def __init__(self, sn_name, sn_subtype, spec_filename, redshift, SNR, spec = None):
         '''
-        initialize object with given arguments
+        instantiation instructions
+
+        Doctests/Examples
+        -----------------
+        >>> s = Spectrum('SN 1997y', 'Ia-norm', 'sn1997y-19970209-uohp.flm', 0.01587, 44.2219)
+        >>> s.name
+        'SN 1997y'
+        >>> s.z
+        0.01587
+        >>> s.spec.shape
+        (3361, 2)
         '''
+
+        # require redshift to be positive
+        assert redshift >= 0
 
         self.name = sn_name
         self.type = sn_subtype
         self.file = spec_filename
         self.z = redshift
-        self.SNR = SNR
-        self.spec = self.read_file()
+        self.SNR = SNR # signal to noise ratio
 
-    def read_file(self):
+        # attempt to make sure spec is in correct format, and read in otherwise
+        if type(spec) != np.ndarray:
+            #self.spec = self.read_file()
+            self.spec = Spectrum.read_file(spec_filename)
+        elif spec.shape[1] != 2:
+            #self.spec = self.read_file()
+            self.spec = Spectrum.read_file(spec_filename)
+        else:
+            self.spec = spec
+
+    def read_file(file):
         '''
-        read spectrum from the given filename and return a 2d array of (column-wise) wavelength, flux
+        reads (and returns) spectrum from provided file (with relative path)
+
+        Parameters
+        ----------
+        file : filename (with relative path) of supernova spectrum (file should have two columns: wavelength flux)
+
+        Returns
+        -------
+        spec : raw spectrum as 2d array of (column-wise) wavelength, flux
+
+        Doctests/Examples
+        -----------------
+        >>> Spectrum.read_file('sn1997y-19970209-uohp.flm').shape
+        (3361, 2)
         '''
         
-        tmp = ascii.read(self.file)
+        tmp = ascii.read(file)
         cols = tmp.colnames
         return np.array([tmp[cols[0]], tmp[cols[1]]]).T
 
-    def dez(self):
+    def dez(spec, z):
         '''
-        de-redshifts (and returns) spectrum
+        de-redshifts (and returns) given spectrum
+
+        Parameters
+        ----------
+        spec : spectrum as 2d array of (column-wise) wavelength, flux
+        z : redshift of SN
+
+        Returns
+        -------
+        spec : de-redshifted spectrum as 2d array of (column-wise) wavelength, flux
+
+        Doctests/Examples
+        -----------------
+        >>> s = Spectrum('SN 1997y', 'Ia-norm', 'sn1997y-19970209-uohp.flm', 0.01587, 44.2219)
+        >>> dz = Spectrum.dez(s.spec, s.z)
+        >>> dz[:,0].max() <= s.spec[:,0].max()
+        True
+        >>> (dz[:,1] == s.spec[:,1]).all()
+        True
         '''
 
-        spec = self.spec
-        wav = spec[:,0] / (1 + self.z)
+        # require redshift to be positive
+        assert z >= 0
+
+        spec = spec
+        wav = spec[:,0] / (1 + z)
         return np.array([wav, spec[:,1]]).T
 
     def sp_medfilt(spec, ksize = 15):
         '''
         wrapper around scipy medfilt function
             applies a median filter (and returns) spectrum
+
+        Parameters
+        ----------
+        spec : spectrum as 2d array of (column-wise) wavelength, flux
+        ksize (optional, odd int) : size of median filter window
+
+        Returns
+        -------
+        spec : median-filtered spectrum as 2d array of (column-wise) wavelength, flux
+
+        Doctests/Examples
+        -----------------
+        >>> s = Spectrum('SN 1997y', 'Ia-norm', 'sn1997y-19970209-uohp.flm', 0.01587, 44.2219)
+        >>> mf = Spectrum.sp_medfilt(s.spec, ksize=45)
+        >>> mf[:,1].std() < s.spec[:,1].std()
+        True
+        >>> (mf[:,0] == s.spec[:,0]).all()
+        True
         '''
 
         flux = medfilt(spec[:,1], ksize)
@@ -111,13 +202,35 @@ class Spectrum:
 
     def log_bin(spec, wav_min = 3100, wav_max = 9600, n_bins = 1024):
         '''
-        bins (and returns) spectrum to logarithmic scale between wav_min and wav_max with n_bins points
+        bins spectrum to logarithmic scale and returns re-binned spectrum
+            flux re-binning utilizes a cubic spline interpolation
+
+        Parameters
+        ----------
+        spec : spectrum as 2d array of (column-wise) wavelength, flux
+        wav_min (optional, int) : minimum of new wavelength scale
+        wav_max (optional, int) : maximum of new wavelength scale
+        n_bins (optional, int) : number of bins in new wavelength scale
+
+        Returns
+        -------
+        spec : re-binned spectrum as 2d array of (column-wise) wavelength, flux
+
+        Doctests/Examples
+        -----------------
+        >>> s = Spectrum('SN 1997y', 'Ia-norm', 'sn1997y-19970209-uohp.flm', 0.01587, 44.2219)
+        >>> lb = Spectrum.log_bin(s.spec, n_bins = 1024)
+        >>> lb.shape
+        (1024, 2)
         '''
 
+        # compute new wavelength array
         dl = np.log(wav_max / wav_min) / n_bins
         n = np.arange(0.5, n_bins + 0.5)
-        wav = spec[:,0]
         log_wav = wav_min * np.exp(n * dl)
+
+        # find flux at new wavelength points (set flux to zero for wavelengths outside original range)
+        wav = spec[:,0]
         spl = CubicSpline(wav, spec[:,1])
         flux = spl(log_wav)
         flux[np.logical_or(log_wav < wav.min(), log_wav > wav.max())] = 0
@@ -125,14 +238,32 @@ class Spectrum:
 
     def cont_subtr(spec, n_spline_pts = 13, ret_cont = False):
         '''
-        fits a cubic spline to a subset of points in the spectrum and subtracts it out to remove a pseudo continuum
+        fits cubic spline to subset of points in given spectrum and subtracts it to remove a pseudo continuum
+
+        Parameters
+        ----------
+        spec : spectrum as 2d array of (column-wise) wavelength, flux
+        n_spline_pts (optional, int) : number of (evenly spaced) points to use for spline fit to spectrum
+        ret_cont (optional, bool) : returns fitted spline with corrected spectrum if True
+
+        Returns
+        -------
+        spec : continuum-subtracted spectrum as 2d array of (column-wise) wavelength, flux
+        spl (if ret_cont is True) : fitted spline
+
+        Doctests/Examples
+        -----------------
+        >>> s = Spectrum('SN 1997y', 'Ia-norm', 'sn1997y-19970209-uohp.flm', 0.01587, 44.2219)
+        >>> Spectrum.cont_subtr(s.spec).shape[1]
+        2
         '''
 
-        wav = spec[:,0]
-        #loc = int(len(wav)/(2*n_spline_pts))
+        # compute indices corresponding to points to fit spline
         spl_indices = np.linspace(0, spec.shape[0] - 1, n_spline_pts, dtype=int)
-        #spl = CubicSpline(wav[loc:-loc:2*loc], spec[loc:-loc:2*loc,1])
+        
+        # fit spline and continuum subtract
         spl = UnivariateSpline(spec[spl_indices,0], spec[spl_indices,1])
+        wav = spec[:,0]
         flux = spec[:,1] - spl(wav)
         if ret_cont is True:
             return np.array([wav, flux]).T, spl
@@ -141,46 +272,114 @@ class Spectrum:
 
     def apodize(spec, end_pct = 0.05):
         '''
-        tapers the edges of the spectrum using a hanning window
+        tapers edges of spectrum using a Hanning window
+
+        Parameters
+        ----------
+        spec : spectrum as 2d array of (column-wise) wavelength, flux
+        end_pct (optional, float between 0 and 1) : percentage of each end of spectrum to taper
+
+        Returns
+        -------
+        spec : apodized spectrum as 2d array of (column-wise) wavelength, flux
+
+        Doctests/Examples
+        -----------------
+        >>> s = Spectrum('SN 1997y', 'Ia-norm', 'sn1997y-19970209-uohp.flm', 0.01587, 44.2219)
+        >>> a = Spectrum.apodize(s.spec, end_pct = 0.05)
+        >>> np.abs(a[0,1]) < np.abs(s.spec[0,1])
+        True
+        >>> np.abs(a[-1,1]) < np.abs(s.spec[-1,1])
+        True
+        >>> a[int(a.shape[0]/2),1] == s.spec[int(a.shape[0]/2), 1]
+        True
         '''
 
         size = int(end_pct * spec.shape[0])
         h = np.hanning(2 * size)
+
+        # form full window by wrapping tapered edges around un-tapered body
         window = np.concatenate((h[:size], np.ones(spec.shape[0] - 2 * size), h[-size:]))
+
         flux = spec[:,1] * window
         return np.array([spec[:,0], flux]).T
 
     def flux_norm(spec):
         '''
-        normalizes the flux (f) according to the algorithm: (f - f_min) / (f_max - f_min) - mean(qty on left)
+        normalizes flux (f) according to the algorithm: (f - f_min) / (f_max - f_min) - mean(qty on left)
+
+        Parameters
+        ----------
+        spec : spectrum as 2d array of (column-wise) wavelength, flux
+
+        Returns
+        -------
+        spec : normalized spectrum as 2d array of (column-wise) wavelength, flux
+
+        Doctests/Examples
+        -----------------
+        >>> s = Spectrum('SN 1997y', 'Ia-norm', 'sn1997y-19970209-uohp.flm', 0.01587, 44.2219)
+        >>> fn = Spectrum.flux_norm(s.spec)
+        >>> np.abs(fn[:,1].mean()) < 0.001 # tolerance on being close enough to zero
+        True
+        >>> fn[:,1].max() < 1
+        True
+        >>> fn[:,1].min() > -1
+        True
         '''
 
         f = spec[:,1]
         nflux = (f - f.min()) / (f.max() - f.min())
         return np.array([spec[:,0], nflux - nflux.mean()]).T
 
-    def preprocess(self, ksize = 15, n_spline_pts = 13, wav_min = 3100, wav_max = 9600, n_bins = 1024):
+    def preprocess(self, ksize = 15, n_spline_pts = 13, wav_min = 3100, wav_max = 9600, n_bins = 1024, end_pct = 0.05):
         '''
-        perform preprocessing procedure on spectrum and return a 2d array of (column-wise) wavelength, flux
+        performs preprocessing procedure on instance's spectrum:
+            de-redshifts, median filters, removes pseudo continuum, normalizes, apodizes edges, bins to logarithmic wavelength bins
+
+        Parameters
+        ----------
+        (object instance)
+        ksize (optional, odd int) : size of median filter window
+        n_spline_pts (optional, int) : number of (evenly spaced) points to use for spline fit to spectrum
+        wav_min (optional, int) : minimum of new wavelength scale
+        wav_max (optional, int) : maximum of new wavelength scale
+        n_bins (optional, int) : number of bins in new wavelength scale
+        end_pct (optional, float between 0 and 1) : percentage of each end of spectrum to taper
+
+        Returns
+        -------
+        spec : fully preprocessed spectrum as 2d array of (column-wise) wavelength, flux
+
+        Doctests/Examples
+        -----------------
+        >>> s = Spectrum('SN 1997y', 'Ia-norm', 'sn1997y-19970209-uohp.flm', 0.01587, 44.2219)
+        >>> p = s.preprocess(n_bins = 1024)
+        >>> p.shape
+        (1024, 2)
+        >>> p[:,1].max() < 1
+        True
+        >>> p[:,1].min() > -1
+        True
         '''
 
         # de-redshift
-        spec = self.dez()
+        spec = Spectrum.dez(self.spec, self.z)
 
         # do median filtering
-        spec = sp_medfilt(spec, ksize = ksize)
+        spec = Spectrum.sp_medfilt(spec, ksize = ksize)
 
         # remove pseudo continuum
-        spec = cont_subtr(spec, n_spline_pts = n_spline_pts)
+        spec = Spectrum.cont_subtr(spec, n_spline_pts = n_spline_pts)
 
         # normalize
-        spec = flux_norm(spec)
+        spec = Spectrum.flux_norm(spec)
 
-        # apodize spectrum
-        spec = apodize(spec)
+        # apodize
+        spec = Spectrum.apodize(spec, end_pct = end_pct)
 
         # bin to logarithmic wavelength bins
-        spec = log_bin(spec, wav_min = wav_min, wav_max = wav_max, n_bins = n_bins)
+        spec = Spectrum.log_bin(spec, wav_min = wav_min, wav_max = wav_max, n_bins = n_bins)
 
         return spec
 
@@ -219,6 +418,6 @@ def main(query = None, query_res_fl = 'query_results.pkl'):
 
 
 # do tests
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+#if __name__ == "__main__":
+#    import doctest
+#    doctest.testmod()
